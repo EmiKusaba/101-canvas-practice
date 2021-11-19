5200056deefbad1edbb80f6b5e169c52fbb2f8bb
EmiKusaba <eeemi615@gmail.com>
2021-11-19 07:57:41 -0600

    added rectangle and line

--- canvas.js
+++ canvas.js
@@ -2,7 +2,21 @@ let canvas = document.getElementById("my-canvas")
 
 console.log(canvas)
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+const c = canvas.getContext('2d')
+
+c.fillRect(75, 75, 100, 100)
+
+c.fillStyle = 'red';
+c.fillRect(125, 125, 200, 40)
+
+// draw line
+c.beginPath();
+c.moveTo(20, 100)
+c.lineTo(1000, 500)
+c.stroke()
 // window.onclick = () => {
 //   for() {
 
